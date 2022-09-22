3b7afcd0689577404649d1e9183fc96974a2ff23
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-09-22 19:37:01 +0200

    Добавьте файлы проекта.

---
+++ Численные методы l Дихотомия/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Численные_методы_l_Дихотомия
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
