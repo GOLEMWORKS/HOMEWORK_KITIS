471daa2c045d01e0a6af76068bd74016d878114f
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-09-27 17:12:04 +0200

    метод хорд файл

---
+++ Численные методы l Метод хорд/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Численные_методы_l_Метод_хорд
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
