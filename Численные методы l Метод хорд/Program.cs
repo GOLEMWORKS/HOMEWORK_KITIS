62bc9be1492ca2bbbc7bba3c1fdf40049597a355
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-09-27 17:35:37 +0200

    Болванка без логики

--- Численные методы l Метод хорд/Program.cs
+++ Численные методы l Метод хорд/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Clear();
+
+                double beg, end;
+
+                Console.WriteLine("Введите начало промежутка: ");
+                beg = IsNumber(Console.ReadLine());
+
+                Console.WriteLine("Введите конец промежутка: ");
+                end = IsNumber(Console.ReadLine());
+
+                if (beg > end)
+                {
+                    Console.WriteLine("Начал промежутка не может быть больше, чем его конец!");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("Уравнение: x^3-cos(x); Точность: 0,0001");
+
+                Calculations(beg, end);
+
+                Console.ReadLine();
+            }
+        }
+
+        static double IsNumber(string str)
+        {
+            if (double.TryParse(str, out double result))
+            {
+                return result;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        static void Calculations(double beg, double end, double accuracy = 0.0001, double x = 0)
+        {
+            
         }
     }
 }
