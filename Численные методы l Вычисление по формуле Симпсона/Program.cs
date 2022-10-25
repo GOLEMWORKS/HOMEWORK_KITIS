a7fcc5aab18020245a60c169532c924081181bbc
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-10-25 19:42:12 +0200

    Создание

---
+++ Численные методы l Вычисление по формуле Симпсона/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Численные_методы_l_Вычисление_по_формуле_Симпсона
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tРешение интеграла по формуле Симпсона");
+            Console.WriteLine("\nПример: dx/(1+x^2)  h = 0,25");
+        }
+    }
+}
