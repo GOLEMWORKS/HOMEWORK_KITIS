d61d1a689a85a801734a1361ffc8d2dfe8b4f2e8
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-10-25 20:48:56 +0200

    gotovo

--- Численные методы l Вычисление по формуле Симпсона/Program.cs
+++ Численные методы l Вычисление по формуле Симпсона/Program.cs
@@ -5,7 +5,48 @@
         static void Main(string[] args)
         {
             Console.WriteLine("\t\tРешение интеграла по формуле Симпсона");
-            Console.WriteLine("\nПример: dx/(1+x^2)  h = 0,25");
+            Console.WriteLine("\nПример: dx/(1+x^2) | h = 0,25; a = 0; b = 2");
+
+            Integral(0.25);
+        }
+
+        static void Integral(double h, int count = 0, double res = 0, double x = 0)
+        {
+            double y;
+            int pos;
+
+            count++;
+
+            y = 0;
+
+            if (count % 2 == 0)
+            {
+                pos = 4;
+            }
+            else if (count % 3 == 0)
+            {
+                pos = 2;
+            }
+            else
+            {
+                pos = 1;
+            }
+
+            if (x < 1)
+            {
+                y = pos / 1 + Math.Pow(x, 2);
+                x += h;   
+            }
+            else
+            {
+                res = res * h/ 3;
+                Console.WriteLine(res);
+                return;
+            }
+
+            res += y;
+
+            Integral(h, count, res, x);
         }
     }
 }
